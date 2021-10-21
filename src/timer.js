9898f49eb5e4c0135e8a90475b944e3d9b016185
vilenavilena <vilena.niyaz@gmail.com>
2021-10-21 21:26:05 +0500

    Almost done homework

--- src/timer.js
+++ src/timer.js
@@ -1,24 +1,31 @@
 const input = document.getElementById('input');
 const blockTime = document.querySelector('.time');
 let interval;
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+
 
 blockTime.innerHTML = 0;
 input.value = 0;
+const end = () => {
+    alert('end');
+}
 
 document.getElementById('start').addEventListener('click', () => {
     if (input.value < 0) {
         input.value = 0;
-        blockTime.innerHTML = 0;
-    }
+        blockTime.innerHTML = 0;   
+    } else {
+        clearInterval(interval);
 
+    }
     blockTime.innerHTML = input.value;
+    interval = setInterval(subtractTime, 1000,);
     
-    clearInterval(interval);
-    interval = setInterval(subtractTime, 1000);
 });
 
 document.getElementById('stop').addEventListener('click', () => {
     clearInterval(interval);
+
 });
 
 document.getElementById('reset').addEventListener('click', () => {
@@ -36,5 +43,7 @@ function subtractTime() {
         }
     } else {
         clearInterval(interval);
+        audio.play();
     }
 }
+
