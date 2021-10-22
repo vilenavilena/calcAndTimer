1ec90f9798d7682c117a2c277febfef755e52bf3
vilenavilena <vilena.niyaz@gmail.com>
2021-10-22 18:38:37 +0500

    Lesson1 homework

--- src/datecalc.js
+++ src/datecalc.js
@@ -1,16 +1,16 @@
-import { DateTime } from "./luxon.js"; // 1
+import { DateTime } from "./luxon.js"; 
 
 export function diffDates(firstDate, secondDate) { 
     firstDate = DateTime.fromISO(firstDate);
     secondDate = DateTime.fromISO(secondDate);
 
     if (firstDate > secondDate)
-        secondDate = [firstDate, firstDate = secondDate][0]; // 2
+        secondDate = [firstDate, firstDate = secondDate][0]; 
 
     return secondDate.diff(firstDate, ['years', 'months', 'days']).toObject();
 }
 
-// 3
+
 export const diffToHtml = diff => `
     <span> 
         ${diff.years ? 'Лет: ' + diff.years : ''} 
