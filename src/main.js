17dab541ce01488695a41b01f9cfd91459061e89
vilenavilena <vilena.niyaz@gmail.com>
2021-11-02 17:26:47 +0500

    Подлючила модули, осталось подключить howler

--- src/main.js
+++ src/main.js
@@ -1,23 +1,6 @@
-import { diffDates, diffToHtml } from "./datecalc.js"; 
-import { formatError } from "./utils.js"; 
+import "./date.js";
+import "./datecalc.js";
+import "./tabs.js";
+import "./timer.js"
 
-
-const dateCalcForm = document.getElementById("datecalc");
-const dateCalcResult = document.getElementById("datecalc__result");
-
-dateCalcForm.addEventListener("submit", handleCalcDates);
-
-function handleCalcDates(event) {
-    dateCalcResult.innerHTML = "";
-    event.preventDefault();
-
-    let { firstDate, secondDate } = event.target.elements;
-    firstDate = firstDate.value, secondDate = secondDate.value;
-
-    if (firstDate && secondDate) {
-        const diff = diffDates(firstDate, secondDate); 
-        dateCalcResult.innerHTML = diffToHtml(diff); 
-    }
-    else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); 
-}
 
